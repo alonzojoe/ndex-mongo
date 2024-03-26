ea5352ecf12c6382772150add018d8eb90d4f39b
Joenell Alonzo <72643848+alonzojoe@users.noreply.github.com>
2024-03-26 16:21:28 +0800

    debug mongodb connection

--- server.js
+++ server.js
@@ -1,10 +1,28 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 const app = express();
+
+dotenv.config();
 
 app.get("/", (req, res) => {
   res.send("Hello Node Express API");
 });
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+app.get("/blog", (req, res) => {
+  res.send("Hello Blog");
 });
+
+app.get("/hello", (req, res) => {
+  res.send("Hello hello");
+});
+
+mongoose
+  .connect(`${process.env.MONGODB_URL}/dbserver`)
+  .then(() => {
+    console.log("MongoDB connected successfully");
+    app.listen(process.env.PORT, () => {
+      console.log("Server is running on port 3000");
+    });
+  })
+  .catch((error) => console.error("Failed to connect to MongoDB:", error));
