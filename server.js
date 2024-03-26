3ddbd523c210d3b2b4448964c04b41d6981c41be
Joenell Alonzo <72643848+alonzojoe@users.noreply.github.com>
2024-03-26 17:02:40 +0800

    Add create product,get all products ,by id and update product,

--- server.js
+++ server.js
@@ -20,12 +20,44 @@ app.get("/hello", (req, res) => {
   res.send("Hello hello");
 });
 
-app.post("/product", async (req, res) => {
+app.get("/products", async (req, res) => {
+  try {
+    const products = await Product.find();
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.get("/products/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.post("/products", async (req, res) => {
   try {
     const product = await Product.create(req.body);
     res.status(201).json(product);
   } catch (error) {
     console.log(error.message);
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.put("/products/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByIdAndUpdate(id, req.body);
+    if (!product) {
+      return res.status(404).json({ message: "Product Not Found" });
+    }
+    res.status(200).json(product);
+  } catch (error) {
     res.status(500).json({ message: error.message });
   }
 });
