b3df13c38df68b5a47441b4e337a36f48841abcd
Joenell Alonzo <72643848+alonzojoe@users.noreply.github.com>
2024-03-27 14:44:48 +0800

    update env and create errorMessage middleware and test it with sample route / throw new error

---
+++ middleware/errorMiddleware.js
@@ -0,0 +1,11 @@
+const errorMiddleWare = (err, req, res, next) => {
+  console.log("Error Middleware Called");
+  const statusCode = res.statusCode ? res.statusCode : 500;
+  res.status(statusCode);
+  res.json({
+    message: err.message,
+    stack: process.env.NODE_ENV === "development" ? err.stack : null,
+  });
+};
+
+module.exports = errorMiddleWare;
--- server.js
+++ server.js
@@ -3,13 +3,18 @@ const dotenv = require("dotenv");
 const connectDB = require("./database/database");
 const productsRouter = require("./routes/products.router");
 const formMiddleware = require("./middleware/formMiddleware");
+const errorMiddleWare = require("./middleware/errorMiddleware");
 
 const app = express();
 app.use(formMiddleware);
+
 dotenv.config();
 
+app.get("/", () => {
+  throw new Error("fake error test errorMiddleware");
+});
 app.use("/api/v1/products", productsRouter);
-
+app.use(errorMiddleWare);
 connectDB();
 
 const PORT = process.env.PORT || 3000;
